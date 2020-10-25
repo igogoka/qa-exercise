14c0f2b3b0202820f021235c3f84d475a9b4905a
igogoka <ihor.nedashkivskiy1@gmail.com>
2020-10-25 21:34:37 +0200

    Add README.md

---
+++ valtech-test_task/README.md
@@ -0,0 +1,10 @@
+To run the test you need:
+ - install JDK 8;
+ - install Maven;
+ 
+ To run tests:
+ - go to root directory;
+ - command : mvn clean test;
+ 
+ To run tests for specific browser (chrome/firefox/edge) execute:
+ - mvn clean test -Dbrowser=chrome; 
--- valtech-test_task/src/test/java/com/TestRunner.java
+++ valtech-test_task/src/test/java/com/TestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features", glue = {"com.steps"}, plugin = {"pretty", "html:target/cucumber-reports/cucumber.html"})
-public class Runner extends AbstractTestNGCucumberTests {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
